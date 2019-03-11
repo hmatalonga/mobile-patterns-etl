6194dc62ae34fa1950ac5509d82a17eb4ed42432
Hugo Matalonga <hmatalonga@gmail.com>
2019-03-11 11:36:27 +0000

    feat: add more classifiers

--- run_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-NOTEBOOKS_DIR=${1:-$PWD}
-DEPS=$(cat requirements.txt | tr '\r\n' ' ')
-BASH_SCRIPT="/opt/conda/bin/conda install $DEPS -y --quiet && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
-
-docker run --rm -it -v "$NOTEBOOKS_DIR":/opt/notebooks -p 8888:8888 continuumio/anaconda3 sh -c "$BASH_SCRIPT"
