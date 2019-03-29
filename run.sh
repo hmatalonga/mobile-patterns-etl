b8056999dd6248f6b531246c5b51c6fd520fcda1
Hugo Matalonga <hmatalonga@gmail.com>
2019-03-29 14:54:00 +0000

    chore: add named container

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ NOTEBOOKS_DIR=${1:-$PWD}
 DEPS=$(cat requirements.txt | tr '\r\n' ' ')
 BASH_SCRIPT="/opt/conda/bin/conda install $DEPS -y --quiet && /opt/conda/bin/jupyter lab --ip='0.0.0.0' --port=8888 --notebook-dir=/opt/notebooks --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''"
 
-docker run --rm -d -v "$NOTEBOOKS_DIR":/opt/notebooks -p 8888:8888 continuumio/anaconda3 sh -c "$BASH_SCRIPT"
+docker run --name mobile-etl --rm -dt -v "$NOTEBOOKS_DIR":/opt/notebooks -p 8888:8888 continuumio/anaconda3 sh -c "$BASH_SCRIPT"
